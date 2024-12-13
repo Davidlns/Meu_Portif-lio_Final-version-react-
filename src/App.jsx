635683d53e6122e831313c268a48012d579146ee
David Lins Do Amaral <linsdavid476@gmail.com>
2024-12-12 21:17:44 -0300

    New

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Contato from './pages/Contato/contato.jsx';
+import Footer from './pages/Footer/footer.jsx';
 import Formacoes from './pages/Formacoes/formacoes.jsx';
 import Home from './pages/Home/home.jsx';
 import Nav from './pages/menunav/nav.jsx';
@@ -15,6 +16,7 @@ function App() {
       <section id="formacoes"><Formacoes /></section>
       <section id="projetos"><Projetos /></section>
       <section id="contato"><Contato /></section>
+      <Footer />
     </div>
   );
 }
